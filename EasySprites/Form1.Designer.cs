91a0d6d89337d7335b9b1eb7df0e16168762819a
Kirill <47493442+KirillBondarBn@users.noreply.github.com>
2019-05-15 02:03:26 +0700

    Snapshot 1a2b

--- EasySprites/Form1.Designer.cs
+++ EasySprites/Form1.Designer.cs
@@ -35,20 +35,34 @@
             this.saveCtrlSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolsBox = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.mainColor = new System.Windows.Forms.PictureBox();
             this.secondColor = new System.Windows.Forms.PictureBox();
             this.penTool = new System.Windows.Forms.PictureBox();
             this.mainImage = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ColorBox = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.toolsBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mainColor)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.secondColor)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.penTool)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mainImage)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ColorBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -114,6 +128,18 @@
             this.toolsBox.TabStop = false;
             this.toolsBox.Text = "Tools";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::EasySprites.Properties.Resources.refresh_button;
+            this.pictureBox1.Location = new System.Drawing.Point(32, 353);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(15, 15);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // pictureBox2
             // 
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
@@ -136,6 +162,7 @@
             this.mainColor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.mainColor.TabIndex = 2;
             this.mainColor.TabStop = false;
+            this.mainColor.Click += new System.EventHandler(this.mainColor_Click);
             // 
             // secondColor
             // 
@@ -169,26 +196,135 @@
             this.mainImage.Size = new System.Drawing.Size(430, 399);
             this.mainImage.TabIndex = 1;
             this.mainImage.TabStop = false;
+            this.mainImage.Click += new System.EventHandler(this.mainImage_Click);
+            this.mainImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainImage_MouseDown);
             this.mainImage.MouseEnter += new System.EventHandler(this.mainImage_MouseEnter);
             this.mainImage.MouseLeave += new System.EventHandler(this.mainImage_MouseLeave);
             this.mainImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.mainImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mainImage_MouseUp);
             // 
-            // pictureBox1
+            // ColorBox
             // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::EasySprites.Properties.Resources.refresh_button;
-            this.pictureBox1.Location = new System.Drawing.Point(32, 353);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(15, 15);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.ColorBox.Controls.Add(this.textBox3);
+            this.ColorBox.Controls.Add(this.textBox2);
+            this.ColorBox.Controls.Add(this.textBox1);
+            this.ColorBox.Controls.Add(this.label3);
+            this.ColorBox.Controls.Add(this.label2);
+            this.ColorBox.Controls.Add(this.label1);
+            this.ColorBox.Controls.Add(this.trackBar3);
+            this.ColorBox.Controls.Add(this.trackBar2);
+            this.ColorBox.Controls.Add(this.trackBar1);
+            this.ColorBox.Font = new System.Drawing.Font("Fedra Sans Pro Bold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ColorBox.ForeColor = System.Drawing.Color.White;
+            this.ColorBox.Location = new System.Drawing.Point(536, 218);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(200, 220);
+            this.ColorBox.TabIndex = 3;
+            this.ColorBox.TabStop = false;
+            this.ColorBox.Text = "Color";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(6, 143);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 23);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.Location = new System.Drawing.Point(6, 167);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 23);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.AutoSize = false;
+            this.trackBar3.Location = new System.Drawing.Point(6, 191);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 23);
+            this.trackBar3.TabIndex = 2;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(114, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(114, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(114, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "B";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(134, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 23);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(134, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 23);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(134, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(27, 23);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ColorBox);
             this.Controls.Add(this.toolsBox);
             this.Controls.Add(this.mainImage);
             this.Controls.Add(this.menuStrip1);
@@ -200,12 +336,17 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.toolsBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.mainColor)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.secondColor)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.penTool)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.mainImage)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ColorBox.ResumeLayout(false);
+            this.ColorBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,6 +366,16 @@
         private System.Windows.Forms.PictureBox secondColor;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox ColorBox;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- EasySprites/Form1.cs
+++ EasySprites/Form1.cs
@@ -13,11 +13,12 @@ namespace EasySprites
 {
     public partial class Form1 : Form
     {
-        Bitmap toolsBmp;
-        Graphics gr;
+        Bitmap toolsBmp, mainBmp;
+        Graphics gr, midGr;
         Pen pen;
         Brush brush = new SolidBrush(Color.Black);
         int MouseX, MouseY;
+        bool draw = false;
 
         public Form1()
         {
@@ -40,16 +41,29 @@ namespace EasySprites
         private void Form1_Load(object sender, EventArgs e)
         {
             toolsBmp = new Bitmap(mainImage.Width, mainImage.Height);
+            mainBmp = new Bitmap(mainImage.Width, mainImage.Height);
             gr = Graphics.FromImage(toolsBmp);
+            midGr = Graphics.FromImage(mainBmp);
             pen = new Pen(brush, 5);
 
             this.BackColor = ColorTranslator.FromHtml("#202230");
+
             toolsBox.BackColor = ColorTranslator.FromHtml("#282a3b");
+            ColorBox.BackColor = ColorTranslator.FromHtml("#282a3b");
+
             menuStrip1.BackColor = ColorTranslator.FromHtml("#282a3b");
             fileToolStripMenuItem.BackColor = ColorTranslator.FromHtml("#282a3b");
             openCtrlOToolStripMenuItem.BackColor = ColorTranslator.FromHtml("#282a3b");
             saveCtrlSToolStripMenuItem.BackColor = ColorTranslator.FromHtml("#282a3b");
             saveToolStripMenuItem.BackColor = ColorTranslator.FromHtml("#282a3b");
+
+            label1.ForeColor = Color.FromArgb(255, 137, 63, 69);
+            label2.ForeColor = Color.FromArgb(255, 59, 176, 143);
+            label3.ForeColor = Color.FromArgb(255, 93, 118, 203);
+
+            trackBar1.BackColor = Color.FromArgb(255, 137, 63, 69);
+            trackBar2.BackColor = Color.FromArgb(255, 59, 176, 143);
+            trackBar3.BackColor = Color.FromArgb(255, 93, 118, 203);
         }
 
 
@@ -63,9 +77,9 @@ namespace EasySprites
             fileToolStripMenuItem.ForeColor = Color.White;
         }
 
-        int x, y;
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
+            int x, y;
             brush = new SolidBrush(mainColor.BackColor);
             if (MouseX >= (MousePosition.X - this.Location.X - mainImage.Location.X - 8) + 5 
             ||  MouseX <= (MousePosition.X - this.Location.X - mainImage.Location.X - 8) - 5)
@@ -78,11 +92,122 @@ namespace EasySprites
             x = MouseX;
             y = MouseY;
 
-            gr.Clear(Color.White);
+            gr.Clear(Color.FromArgb(0, 0, 0, 0));
 
-            gr.FillRectangle(brush, x, y, 15, 15);
+            pen_Tool(brush, x, y);
 
             mainImage.Image = toolsBmp;
+            mainImage.BackgroundImage = mainBmp;
+        }
+
+        public void pen_Tool(Brush brush, int x, int y)
+        {
+            pen = new Pen(Color.Black);
+            if(draw)
+            {
+                midGr.FillRectangle(brush, x, y, 15, 15);
+                gr.DrawRectangle(pen, x - 1, y - 1, 16, 16);
+                gr.FillRectangle(brush, x, y, 15, 15);
+            }
+            else
+            {
+                gr.DrawRectangle(pen, x - 1, y - 1, 16, 16);
+                gr.FillRectangle(brush, x, y, 15, 15);
+            }
+        }
+
+        private void mainImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void mainImage_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            brush = new SolidBrush(mainColor.BackColor);
+            if (MouseX >= (MousePosition.X - this.Location.X - mainImage.Location.X - 8) + 5
+            || MouseX <= (MousePosition.X - this.Location.X - mainImage.Location.X - 8) - 5)
+                MouseX = MousePosition.X - this.Location.X - mainImage.Location.X - 8;
+
+            if (MouseY >= (MousePosition.Y - this.Location.Y - mainImage.Location.Y - 31) + 5
+            || MouseY <= (MousePosition.Y - this.Location.Y - mainImage.Location.Y - 31) - 5)
+                MouseY = MousePosition.Y - this.Location.Y - mainImage.Location.Y - 31;
+
+            x = MouseX;
+            y = MouseY;
+
+            gr.Clear(Color.FromArgb(0, 0, 0, 0));
+
+            pen_Tool(brush, x, y);
+
+            mainImage.Image = toolsBmp;
+            mainImage.BackgroundImage = mainBmp;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Color clr = secondColor.BackColor;
+            secondColor.BackColor = mainColor.BackColor;
+            mainColor.BackColor = clr;
+        }
+
+        private void mainColor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            byte r = Convert.ToByte(trackBar1.Value),
+               g = Convert.ToByte(trackBar2.Value),
+               b = Convert.ToByte(trackBar3.Value);
+
+            mainColor.BackColor = Color.FromArgb(255, r, g, b);
+
+            textBox1.Text = trackBar1.Value.ToString();
+            textBox2.Text = trackBar2.Value.ToString();
+            textBox3.Text = trackBar3.Value.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = Convert.ToInt32(textBox1.Text);
+                if (num > 255) textBox1.Text = "255";
+                trackBar1.Value = num;
+                trackBar3_Scroll(sender, e);
+            }
+            catch { }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = Convert.ToInt32(textBox2.Text);
+                if (num > 255) textBox2.Text = "255";
+                trackBar2.Value = num;
+                trackBar3_Scroll(sender, e);
+            }
+            catch { }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = Convert.ToInt32(textBox3.Text);
+                if (num > 255) textBox2.Text = "255";
+                trackBar3.Value = num;
+                trackBar3_Scroll(sender, e);
+            }
+            catch { }
+        }
+
+        private void mainImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            draw = true;
         }
 
         private void mainImage_MouseEnter(object sender, EventArgs e)
@@ -95,6 +220,7 @@ namespace EasySprites
         private void mainImage_MouseLeave(object sender, EventArgs e)
         {
             Cursor.Show();
+            gr.Clear(Color.FromArgb(0, 0, 0, 0));
         }
     }
 }
